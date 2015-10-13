b0b94979739243d095ae0371fe7c76a356f1d231
opensourcegeek_axivity <praveen@axivity.com>
2015-10-13 17:39:53 +0100

    Amended server to add insturctions on running server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 /**
  * Created by Praveen on 07/09/2015.
  */
+
+/*
+* To run this from console execute following command,
+*   `UV_THREADPOOL_SIZE=100 node server.js`
+* this helps work around the issue with node
+* */
+
 require("babel/register")({
     retainLines: true
 });
