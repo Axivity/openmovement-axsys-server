91920d0a1c48ebadfe7c4164086cc1e95fec734d
opensourcegeek_axivity <praveen@axivity.com>
2015-10-15 15:59:17 +0100

    Broken client

--- client-lib/client.js
+++ client-lib/client.js
@@ -2,143 +2,55 @@
  * Created by Praveen on 08/09/2015.
  */
 
-import * as io from 'socket.io-client';
-
-import { hasError, getData, getError } from '../lib/api/payload';
+import DeviceAPI from './device-api';
+import ConnectionAPI from './connection-api';
+import WebSocketConnection from './websocket-connection';
 import * as constants from '../lib/services/event-name-constants';
-import * as binUtils from '../lib/utils/binary';
 
 var AX = window.AX || {};
 
-
-function WebSocketConnection(onConnected, clientKey) {
-
-    var ws = new WebSocket('ws://localhost:9693/');
-    ws.binaryType = "arraybuffer";
-
-    var self = this;
-
-    self.clientKey = clientKey || "default-key";
-
-    self.callbacks = {};
-
-    ws.onmessage = function(event) {
-        let msg = JSON.parse(event.data);
-        console.log(msg);
-
-        if(msg) {
-            let eventName = msg.event;
-            console.log(eventName);
-
-            let fns = self.callbacks[eventName];
-
-
-            if(fns) {
-                let payload = msg.data;
-                console.log(payload);
-
-                fns.forEach((fn) => {
-                    if(!hasError(payload)) {
-                        fn(getData(payload));
-
-                    }
-                    // TODO: How should the error be propagated??
-                    //else {
-                    //    fn(new Error(), null);
-                    //}
-
-                } );
-
-            } else {
-                console.warn('No callbacks registered for event ' + eventName);
-            }
-
-        }
-
-    };
-
-    ws.onopen = function () {
-        onConnected();
-    };
-
-
-    function addCallbackForEvent(event, callback) {
-        console.log(event);
-        console.log(callback);
-
-        console.log(self.callbacks);
-        if(self.callbacks[event] === undefined) {
-            self.callbacks[event] = [];
-        }
-        self.callbacks[event].push(callback);
+class API {
+    constructor(onDeviceAdded,
+                onDeviceRemoved,
+                onConnected,
+                onDisconnected,
+                onDataReceived,
+                clientKey) {
+        this.onDeviceAdded = onDeviceAdded;
+        this.onDeviceRemoved = onDeviceRemoved;
+        this.onConnected = onConnected;
+        this.onDisconnected = onDisconnected;
+        this.onDataReceived = onDataReceived;
+        this.conn = new WebSocketConnection(clientKey);
+        this.connectedDevices = {};
+        this.__init();
     }
 
-    function send(event, data, callback) {
-        var msg = {
-            'event': event,
-            'data': data
-        };
+    __init() {
+        new ConnectionAPI(
+            this.onDeviceAdded,
+            this.onDeviceRemoved,
+            this.onConnected,
+            this.onDisconnected,
+            this.conn
+        ).init();
 
-        // assumes event name for both sending and receiving data is same
-        addCallbackForEvent(event, callback);
-        ws.send(JSON.stringify(msg));
     }
 
-    return {
-        'addCallbackForEvent': addCallbackForEvent,
-        'send': send
+    connect(options, callbackpath) {
+        let conn = new DeviceAPI(this.onDataReceived, this.conn);
+        this.connectedDevices[path] = conn;
+        conn.connect(options, callback);
     }
 
-}
+    write(options, callback) {
 
-function API(onDeviceAdded,
-             onDeviceRemoved,
-             onConnected,
-             onDisconnected,
-             onDataReceived,
-             clientKey) {
-
-    var conn = new WebSocketConnection(onConnected, clientKey);
-
-    /*
-    *
-    * */
-    this.getDevices = (callback) => {
-        conn.send(constants.AX_CLIENT_DEVICES_GET_ALL, {}, callback);
-    };
+    }
 
 
-    /*
-     * */
-    this.connect = (options, callback) => {
-        conn.send(constants.AX_DEVICE_CONNECT, options, callback);
 
-    };
 
-    /*
-     * */
-    this.write = (options, callback) => {
-        conn.send(constants.AX_DEVICE_WRITE, options, callback);
-    };
 
-    /*
-     * */
-    this.disconnect = (options, callback) => {
-        conn.send(constants.AX_DEVICE_DISCONNECT, options, callback);
-    };
-
-    /*
-     *  Internal Only - called to register any global event handlers
-     **/
-    this.init = () => {
-        // Add a data listener
-        conn.addCallbackForEvent(constants.AX_CLIENT_DATA, (payload) => {
-            // TODO: We could manipulate array buffer here??
-            onDataReceived(payload);
-        });
-    };
-
-    this.init();
 
 }
 
@@ -147,5 +59,4 @@ AX.API = API;
 
 // attach it to global window
 window.AX = AX;
-
 
